6ac145a945c134a707da1869fefdef35f49e221d
yolandalopezfernandez <yolandaa020@outlook.com>
2020-09-12 23:59:41 -0300

    subido

---
+++ Tp1_Yolanda Lopez Fernandez/Aritmetica.h
@@ -0,0 +1,45 @@
+    /** \brief Recibe variables a sumar
+     *
+     * \param  Sumar numeros
+     * \return Resultado suma
+     *
+     */
+
+    int sumarNumeros(int operandoA, int operandoB);
+
+    /** \brief Recibe variables a restar
+     *
+     * \param Restar numeros
+     * \return resultado de resta
+     *
+     */
+
+
+    int restarNumeros(int operandoA, int operandoB);
+
+    /** \brief Recibe variables a dividir
+     *
+     * \param divisir numeros
+     * \return resultado de divsion
+     *
+     */
+
+
+    int dividir(float* resultado,int operandoA,int operandoB);
+
+    /** \brief Recibe variables a multiplicar
+     *
+     * \param multiplicar numeros
+     * \return resultado de multiplicacion
+     *
+     */
+    int multiplicarNumeros(int operandoA, int operandoB);
+
+    /** \brief Recibe variable
+     *
+     * \param factorial de operando
+     * \return resultado de factorial
+     *
+     */
+
+    int CalcularFactorial(int);
